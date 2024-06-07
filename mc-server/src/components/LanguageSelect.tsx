e886a94912abcdf02faecf5b1836f2f72c5d0a6b
DejFcoder <trebatickydavid670@gmail.com>
2024-06-07 20:25:15 +0200

    done footer last

--- mc-server/src/components/LanguageSelect.tsx
+++ mc-server/src/components/LanguageSelect.tsx
@@ -58,19 +58,7 @@ const LanguageSelect: FC<LanguageSelectProps> = ({ selectedLanguage, onLanguageC
         >
           <img src={currentLanguage === "EN" ? EN : currentLanguage === "SK" ? SK : CZ} alt={currentLanguage} className="inline-block h-5 w-8 mr-2" />
           {currentLanguage}
-          <svg
-            className="-mr-1 ml-2 h-5 w-5"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            aria-hidden="true"
-          >
-            <path
-              fillRule="evenodd"
-              d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 011.414 1.414l-4 4a1 1 01-1.414 0l-4-4a1 1 010-1.414z"
-              clipRule="evenodd"
-            />
-          </svg>
+          <i className='bx bx-caret-down text-md ml-1'></i>
         </button>
       </div>
       {dropdownOpen && (
